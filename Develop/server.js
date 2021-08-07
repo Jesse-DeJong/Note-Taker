f088386260ca3f2e37f0b6ec9bbeda4a9e9a3bcf
Jesse De Jong <jdoceau@gmail.com>
2021-08-07 17:53:43 +1000

    wrote terrible but functional POST request to add new notes... currently the getnotes function in the provided index.js does not read the new note immediately but on reload?

--- Develop/server.js
+++ Develop/server.js
@@ -7,6 +7,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Return static HTML assets
 app.use(express.static('public'));
 
@@ -21,12 +24,39 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-/* API GET Routes */
+/* API Routes */
 // GET Route for /api/notes
 app.get('/api/notes', (req, res) => {
   fs.readFile(path.join(__dirname, '/db/db.json'), (error, data) =>
   error ? console.error(error) : res.json(JSON.parse(data))
   );
+});
+
+// POST Route for /api/notes
+app.post('/api/notes', (req, res) => {
+
+  // Read the current DB file and save it (parsed) to a variable
+  fs.readFile(path.join(__dirname, '/db/db.json'), (error, data) => {
+    if (error) {
+      throw error;
+    }  
+    const currentFile = JSON.parse(data); 
+    // Call a function to append the new data
+    updateJSONdb(currentFile);
+  });
+  
+  // Push the new data into the array from the db JSON file
+  function updateJSONdb (currentFile) {
+    currentFile.push(req.body);
+    // Stringify the object to be written back to the file
+    const stringCurrentFile = JSON.stringify(currentFile);
+
+    // Write the updated and stringified file back to db.json
+    fs.writeFile(path.join(__dirname, '/db/db.json'), stringCurrentFile, (err) =>
+    err ? console.error(err) : console.log(`Successfully updated file with Title ${req.body.title} and Text ${req.body.text}`)
+    );
+  };
+
 });
 
 // Open port
