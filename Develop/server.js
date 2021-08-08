d866508a1781ce9c8a5a5c24d78de39ac3ce0d3f
Jesse De Jong <jdoceau@gmail.com>
2021-08-08 17:14:17 +1000

    Added delete route for note removal

--- Develop/server.js
+++ Develop/server.js
@@ -66,6 +66,40 @@ app.post('/api/notes', (req, res) => {
   res.status(201).send(`Successfully updated file with Title ${req.body.title} and Text ${req.body.text}`);
 });
 
+// DELETE Route for /api/notes
+app.delete('/api/notes/:id', (req, res) => {
+
+  // Read the current DB file and save it (parsed) to a variable
+  fs.readFile(path.join(__dirname, '/db/db.json'), (error, data) => {
+    if (error) {
+      throw error;
+    }  
+    const currentFile = JSON.parse(data); 
+    // Call a function to append the new data
+    deleteJSONdb(currentFile);
+  });
+    
+  // Push the new data into the array from the db JSON file
+  function deleteJSONdb (currentFile) {
+
+    const deletionID = req.params.id;
+
+    const removeIndex = currentFile.map(item => item.id).indexOf(deletionID);
+    ~removeIndex && currentFile.splice(removeIndex, 1);
+
+    // Stringify the object to be written back to the file
+    const stringCurrentFile = JSON.stringify(currentFile);
+
+    // Write the updated and stringified file back to db.json
+    fs.writeFile(path.join(__dirname, '/db/db.json'), stringCurrentFile, (err) =>
+    err ? console.error(err) : console.log(
+    `Successfully deleted note with ID ${req.params.id}.`)
+    );
+  };
+  // Return a successful response to the client
+  res.status(200).send(`Successfully deleted note with ID ${req.params.id}.`);
+});
+
 // Open port
 app.listen(PORT, () => {
   console.log(`Note-taking app listening at http://localhost:${PORT}`)
